7d695a763ad67852c6d49e4f960e487f2f151b50
Nolen Johnson <johnsonnolen@gmail.com>
2024-04-27 23:05:00 +0000

    Revert "daisy: Ship full recovery image"
    This reverts commit 51ec069962773489fc3df8221922b00e7a235a7a.
    
    Reason for revert: Global now in vendor/lineage.
    
    Change-Id: Ib5d19fe8e69e52c87454418f9e4a0c4bdf90e7b8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,11 +49,6 @@ endif
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/wakeup_gesture"
 
-ifeq ($(TARGET_DEVICE), sakura)
-# Recovery
-BOARD_USES_FULL_RECOVERY_IMAGE := true
-endif
-
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
