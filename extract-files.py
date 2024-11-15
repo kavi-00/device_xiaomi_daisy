3d80514c0f71b9ef83c7cf73250edc65f439e37c
Giovanni Ricca <giovanniricca@proton.me>
2024-11-15 23:33:24 +0100

    daisy: Migrate to py extract utils
    Change-Id: Ic7a71d021a2436e897496ba2c16ebfeda7e3c383

---
+++ extract-files.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/lib/libchromaflash.so', 'vendor/lib/libarcsoft_high_dynamic_range.so', 'vendor/lib/libdualcameraddm.so', 'vendor/lib/libseemore.so', 'vendor/lib/liboptizoom.so', 'vendor/lib/libubifocus.so', 'vendor/lib/libvidhance.so'): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'vendor/lib64/libgf_hal.so': blob_fixup()
+        .sig_replace('10 03 00 D0 11 52 46 F9', '10 03 00 D0 1F 20 03 D5'),
+    'vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so': blob_fixup()
+        .remove_needed('libprotobuf-cpp-lite.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'daisy',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'msm8953-common', module.vendor)
+    utils.run()
