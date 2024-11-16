85579918ab0e3769356ade4782836d00bb2399b5
Giovanni Ricca <giovanniricca@proton.me>
2024-11-16 11:57:10 +0100

    daisy: Fix gf_fingerprint.goodix.default ELF checks
    Change-Id: If25ff641ad9383a71553bb2cb0548133c362f7c8

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import extract_utils.tools
+
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_18'
+
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
@@ -22,11 +26,13 @@ namespace_imports = [
 blob_fixups: blob_fixups_user_type = {
     ('vendor/lib/libchromaflash.so', 'vendor/lib/libarcsoft_high_dynamic_range.so', 'vendor/lib/libdualcameraddm.so', 'vendor/lib/libseemore.so', 'vendor/lib/liboptizoom.so', 'vendor/lib/libubifocus.so', 'vendor/lib/libvidhance.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'vendor/lib64/hw/gf_fingerprint.goodix.default.so': blob_fixup()
+        .replace_needed('libvendor.goodix.hardware.fingerprint@1.0.so', 'vendor.goodix.hardware.fingerprint@1.0.so'),
     'vendor/lib64/libgf_hal.so': blob_fixup()
         .sig_replace('10 03 00 D0 11 52 46 F9', '10 03 00 D0 1F 20 03 D5'),
     'vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so': blob_fixup()
         .remove_needed('libprotobuf-cpp-lite.so')
-        .binary_regex_replace(b'libvendor.goodix.hardware.fingerprint@1.0.so', b'vendor.goodix.hardware.fingerprint@1.0.so\x00\x00\x00'),
+        .replace_needed('libvendor.goodix.hardware.fingerprint@1.0.so', 'vendor.goodix.hardware.fingerprint@1.0.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
