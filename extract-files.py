aae3a90e1505da580cea7b466e332788a9197ee3
Giovanni Ricca <giovanniricca@proton.me>
2024-11-16 11:35:28 +0100

    daisy: Drop libmmcamera_trueportrait_lib
    Change-Id: Ic21a88a84452613627316856249aa4b1ff11ebf8

--- extract-files.py
+++ extract-files.py
@@ -37,5 +37,7 @@ module = ExtractUtilsModule(
 )
 
 if __name__ == '__main__':
-    utils = ExtractUtils.device_with_common(module, 'msm8953-common', module.vendor)
+    utils = ExtractUtils.device_with_common(
+        module, 'msm8953-common', module.vendor
+    )
     utils.run()
